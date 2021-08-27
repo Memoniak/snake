418243b9e347fa8a461b2d8b6561f81b94909495
Pol Bachelin <pol.bachelin@epitech.eu>
2021-08-26 19:29:31 -0700

    feat(lib): updated lib with scenes

---
+++ lib/ECS/src/Core/Scenes/Manager/SceneManager.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2021
+** redCodeEngine
+** File description:
+** SceneManager
+*/
+
+#include "SceneManager.hpp"
+
+SceneManager::SceneManager(SystemManager &systemManager, EntityManager &entityManager) :
+_entityManager(entityManager), _systemManager(systemManager)
+{
+}
+
+// SceneManager::~SceneManager()
+// {
+// }
+
+void SceneManager::update(float dt)
+{
+    _systemManager.update(dt);
+    _currentScene->update();
+    _entityManager.cleanDestroyedEntities();
+}
